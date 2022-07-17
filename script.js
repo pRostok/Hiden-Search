2d0c46bedd9ebc1907fe37f20a3d5197b5e1df47
pRostok <rostmart29@gmail.com>
2022-07-17 14:52:35 -0700

    finalize script

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ const search = document.querySelector('.search')
 const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
 
-btn.addEventListener()
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
+
